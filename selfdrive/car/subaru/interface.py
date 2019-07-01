150b08dc9a3483e52638b1c1536d01cbb62d0425
Chrispreilly <cpreilly@buffalo.edu>
2019-06-30 22:53:23 -0400

    Update interface.py

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -72,7 +72,7 @@ class CarInterface(object):
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
       
-      #start XV with imprezza values
+      #start crosstrek with imprezza values
     if candidate == CAR.CROSSTREK:
       ret.mass = 1568 + std_cargo
       ret.wheelbase = 2.67
