70cb16154124f0b8b868c980f58efe28b33b49e2
Chrispreilly <cpreilly@buffalo.edu>
2019-07-02 17:34:31 -0400

    Increase actuator delay

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -79,7 +79,7 @@ class CarInterface(object):
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 13
       tire_stiffness_factor = 1.0
-      ret.steerActuatorDelay = 0.2   # end-to-end angle controller
+      ret.steerActuatorDelay = 0.3   # end-to-end angle controller
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1, 0.2], [0.02, 0.03]] # [[0.2, 0.3], [0.02, 0.03]]
