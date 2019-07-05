eff9e5c051aec5ed23ede75278857fda95c72c6e
Chrispreilly <cpreilly@buffalo.edu>
2019-07-05 17:35:40 -0400

    Update interface.py

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -82,7 +82,7 @@ class CarInterface(object):
       ret.steerActuatorDelay = 0.4   # end-to-end angle controller
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20., 30.], [0., 10., 20., 30.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05., 0.2, 0.3, 0.4], [0.02, 0.03, 0.04]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05, 0.2, 0.3, 0.4], [0.02, 0.03, 0.04]]
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
 
