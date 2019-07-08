72b06d2870c48dd1ac6041dc2fb5b5a4be6a3a46
Chrispreilly <cpreilly@buffalo.edu>
2019-07-08 16:24:21 -0400

    update closer to 0.6.0 new panda style

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -1,8 +1,8 @@
-const int SUBARU_MAX_STEER = 3500; //2047; // 1s
+const int SUBARU_MAX_STEER = 3500; // 1s
 // real time torque limit to prevent controls spamming
 // the real time limit is 1500/sec
 const int SUBARU_MAX_RT_DELTA = 940;          // max delta torque allowed for real time checks
-const int32_t SUBARU_RT_INTERVAL = 250000;    // 250ms between real time checks
+const uint32_t SUBARU_RT_INTERVAL = 250000;    // 250ms between real time checks
 const int SUBARU_MAX_RATE_UP = 50;
 const int SUBARU_MAX_RATE_DOWN = 70;
 const int SUBARU_DRIVER_TORQUE_ALLOWANCE = 60;
@@ -17,8 +17,8 @@ int subaru_op_active = 0;
 
 
 static void subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-  int bus_number = (to_push->RDTR >> 4) & 0xFF;
-  uint32_t addr = to_push->RIR >> 21;
+  int bus = GET_BUS(to_push);
+  int addr = GET_ADDR(to_push);
 
   if ((addr == 0x119 || addr == 0x371) && (bus_number == 0)){
     int bit_shift = (addr == 0x119) ? 16 : 29;
@@ -34,7 +34,8 @@ static void subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     int cruise_engaged = (to_push->RDHR >> bit_shift) & 1;
     if (cruise_engaged && !subaru_cruise_engaged_last) {
       controls_allowed = 1;
-    } else if (!cruise_engaged) {
+    }
+    if (!cruise_engaged) {
       controls_allowed = 0;
     }
     subaru_cruise_engaged_last = cruise_engaged;
@@ -42,9 +43,10 @@ static void subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 }
 
 static int subaru_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
-  uint32_t addr = to_send->RIR >> 21;
-
-  // subaru_op_active presence check
+  int tx = 1;
+  int addr = GET_ADDR(to_send);
+  
+    // subaru_op_active presence check
   if (addr == 0x100) {
     subaru_op_active = 1;
   }
@@ -53,7 +55,7 @@ static int subaru_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   if (addr == 0x122 || addr == 0x164) {
     int bit_shift = (addr == 0x122) ? 16 : 8;
     int desired_torque = ((to_send->RDLR >> bit_shift) & 0x1FFF);
-    int violation = 0;
+    bool violation = 0;
     uint32_t ts = TIM2->CNT;
     desired_torque = to_signed(desired_torque, 13);
 
@@ -95,50 +97,35 @@ static int subaru_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
     }
 
     if (violation) {
-      return false;
+      tx = 0;
     }
 
   }
-  return true;
+  return tx;
 }
 
 static int subaru_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 
-  // shifts bits 29 > 11
-  int32_t addr = to_fwd->RIR >> 21;
-
-  // forward CAN 0 > 1
+  int bus_fwd = -1;
   if (bus_num == 0) {
-    return 1; // ES CAN
+    bus_fwd = 1;  // Camera CAN
   }
-  // forward CAN 1 > 0, except ES_LKAS
-  else if (bus_num == 1) {
-
-    // outback 2015
-    if (addr == 0x164) {
-      return -1;
-    }
-
-    // global platform
+  if (bus_num == 1) {
     if (subaru_op_active) {
-      // ES_LKAS
-      if (addr == 0x122) {
-        return -1;
-      }
-       // ES_Distance
-      if (addr == 545) {
-        return -1;
-      }
-      // ES_LKAS_State
-      if (addr == 802) {
-        return -1;
+      // 356 is LKAS for outback 2015
+      // 356 is LKAS for Global Platform
+      // 545 is ES_Distance
+      // 802 is ES_LKAS
+      int addr = GET_ADDR(to_fwd);
+      int block_msg = (addr == 290) || (addr == 356) || (addr == 545) || (addr == 802);
+      if (!block_msg) {
+        bus_fwd = 0;  // Main CAN
       }
     }
-    return 0; // Main CAN
   }
 
   // fallback to do not forward
-  return -1;
+  return bus_fwd;
 }
 
 const safety_hooks subaru_hooks = {
