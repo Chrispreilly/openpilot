09752071fe336a9703e607b1db3a0a508e1051c0
Chrispreilly <cpreilly@buffalo.edu>
2019-07-11 19:50:41 -0400

    cleanup

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -161,10 +161,7 @@ class CarInterface(object):
     ret.gasPressed = self.CS.user_gas_pressed
 
     # cruise state
-    if (self.CS.acc_active == True and self.CS.es_lkas_allowed == True):
-      ret.cruiseState.enabled = True
-    else:
-      ret.cruiseState.enabled = False
+    ret.cruiseState.enabled = bool(self.CS.acc_active)
     ret.cruiseState.speed = self.CS.v_cruise_pcm * CV.KPH_TO_MS
     ret.cruiseState.available = bool(self.CS.main_on)
     ret.cruiseState.speedOffset = 0.
@@ -201,9 +198,9 @@ class CarInterface(object):
     if ret.doorOpen:
       events.append(create_event('doorOpen', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
 
-    if self.CS.acc_active and not self.acc_active_prev and self.CS.es_lkas_allowed:
+    if self.CS.acc_active and not self.acc_active_prev:
       events.append(create_event('pcmEnable', [ET.ENABLE]))
-    if not self.CS.acc_active or not self.CS.es_lkas_allowed:
+    if not self.CS.acc_active::
       events.append(create_event('pcmDisable', [ET.USER_DISABLE]))
 
     # disable on gas pedal rising edge
