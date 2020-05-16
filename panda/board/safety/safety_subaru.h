42f5cd32e6cf8b02e6fad12468eba21a113ebab7
Chrispreilly <cpreilly@buffalo.edu>
2020-05-15 21:44:56 -0400

    continue steering with gas pressed

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -116,7 +116,7 @@ static int subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
       int byte = subaru_global ? 4 : 0;
       bool gas_pressed = GET_BYTE(to_push, byte) != 0;
       if (!unsafe_allow_gas && gas_pressed && !gas_pressed_prev) {
-        controls_allowed = 0;
+        //controls_allowed = 0; //continue stering with gas pressed
       }
       gas_pressed_prev = gas_pressed;
     }
